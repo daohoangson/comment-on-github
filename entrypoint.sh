c538fc91b158b15caa797f6447982ded02875413
Dao Hoang Son <daohoangson@gmail.com>
2020-07-03 22:57:27 +0700

    Execute `create_commit_comment` if pr not found

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,6 @@ require "octokit"
 json = File.read(ENV.fetch("GITHUB_EVENT_PATH"))
 event = JSON.parse(json)
 
-github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
-
 if !ENV["GITHUB_TOKEN"]
   puts "Missing GITHUB_TOKEN"
   exit(1)
@@ -18,22 +16,22 @@ if ARGV[0].empty?
   exit(1)
 end
 
+github = Octokit::Client.new(access_token: ENV["GITHUB_TOKEN"])
+
 message = ARGV[0]
 repo = event["repository"]["full_name"]
 
 if ENV.fetch("GITHUB_EVENT_NAME") == "pull_request"
-  pr_number = event["number"]
+  github.add_comment(repo, event["number"], message)
 else
   pulls = github.pull_requests(repo, state: "open")
 
   push_head = event["after"]
   pr = pulls.find { |pr| pr["head"]["sha"] == push_head }
 
-  if !pr
-    puts "Couldn't find an open pull request for branch with head at #{push_head}."
-    exit(1)
+  if pr
+    github.add_comment(repo, pr["number"], message)
+  else
+    github.create_commit_comment(repo, ENV.fetch("GITHUB_SHA"), message)
   end
-  pr_number = pr["number"]
 end
-
-github.add_comment(repo, pr_number, message)
